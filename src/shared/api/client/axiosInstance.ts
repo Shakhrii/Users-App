fb3473166ddafb56ad6f5c93bf7ebfccb3d25c06
Shakhrii <sh.ramaldanova@gmail.com>
2026-01-25 17:42:15 +0300

    feat: add axios client

---
+++ src/shared/api/client/axiosInstance.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export const axiosInstance = axios.create({
+  baseURL: 'https://6974ad74265838bbea959bb9.mockapi.io/',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${JSON.parse(token)}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
