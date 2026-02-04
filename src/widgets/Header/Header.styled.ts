ddcdb2692763fa585060c5235d731441af7397fe
Shakhrii <sh.ramaldanova@gmail.com>
2026-02-04 11:42:49 +0300

    feat: add header

---
+++ src/widgets/Header/Header.styled.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const HeaderWrapper = styled.header`
+  height: 64px;
+  padding: 0 50px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid #f0f0f0;
+`;
+
+export const Title = styled.h1`
+  margin: 0;
+  font-size: 18px;
+  font-weight: 500;
+`;
--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,5 +1,26 @@
-function Header() {
-  return <header>Header</header>;
-}
+import { Button } from 'antd';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '@shared/lib/hooks/useAuth';
+
+import * as S from './Header.styled';
+
+const Header = () => {
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+
+  const handleClick = () => {
+    logout();
+    navigate('/login');
+  };
+
+  return (
+    <S.HeaderWrapper>
+      <S.Title>Users App</S.Title>
+      <Button type="primary" onClick={handleClick}>
+        Выйти
+      </Button>
+    </S.HeaderWrapper>
+  );
+};
 
 export default Header;
