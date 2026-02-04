c5cda2d074d1cadb28b44081cb5590cdabc67a4a
Shakhrii <sh.ramaldanova@gmail.com>
2026-02-04 11:49:18 +0300

    feat: add global styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,25 +5,29 @@ import LoginPage from '@pages/Login';
 import NotFound from '@pages/NotFound';
 import { QueryProvider } from '@app/providers/QueryProvider';
 import { ProtectedRoute, PublicRoute } from '@features/auth';
+import { GlobalStyle } from './styles/GlobalStyle';
 
 export const App = () => {
   return (
-    <QueryProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<PublicRoute />}>
-            <Route path="/login" element={<LoginPage />} />
-          </Route>
-          <Route element={<MainLayout />}>
-            <Route element={<ProtectedRoute />}>
-              <Route path="/" element={<Users />} />
-              <Route path="/users" element={<Users />} />
+    <>
+      <GlobalStyle />
+      <QueryProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<PublicRoute />}>
+              <Route path="/login" element={<LoginPage />} />
             </Route>
+            <Route element={<MainLayout />}>
+              <Route element={<ProtectedRoute />}>
+                <Route path="/" element={<Users />} />
+                <Route path="/users" element={<Users />} />
+              </Route>
 
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </QueryProvider>
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </QueryProvider>
+    </>
   );
 };
