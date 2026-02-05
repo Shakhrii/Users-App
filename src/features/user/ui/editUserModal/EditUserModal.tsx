88d0b0a968a06e601629f19936300dc0026b3e5d
Shakhrii <sh.ramaldanova@gmail.com>
2026-02-05 14:52:28 +0300

    add deleting user

--- src/features/user/ui/editUserModal/EditUserModal.tsx
+++ src/features/user/ui/editUserModal/EditUserModal.tsx
@@ -1,5 +1,5 @@
-import { useMutationUpdateUser } from '@entities/user/model/queries';
-import { Modal, Form } from 'antd';
+import { useMutationDeleteUser, useMutationUpdateUser } from '@entities/user/model/queries';
+import { Modal, Form, Space, Popconfirm, Button } from 'antd';
 import { UserForm } from '../form/UserForm';
 import { User } from '@entities/user';
 
@@ -11,13 +11,26 @@ type Props = {
 
 export const EditUserModal = ({ open, onClose, initialValues }: Props) => {
   const [form] = Form.useForm();
-  const createMutation = useMutationUpdateUser();
+  const editMutation = useMutationUpdateUser();
+  const deleteMutation = useMutationDeleteUser();
 
   const handleOk = async () => {
     try {
       const values = await form.validateFields();
 
-      createMutation.mutate(values, {
+      editMutation.mutate(values, {
+        onSuccess: onClose,
+      });
+    } catch {
+      console.log('error');
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      const values = await form.validateFields();
+
+      deleteMutation.mutate(values, {
         onSuccess: onClose,
       });
     } catch {
@@ -31,11 +44,28 @@ export const EditUserModal = ({ open, onClose, initialValues }: Props) => {
       open={open}
       onCancel={onClose}
       onOk={handleOk}
-      confirmLoading={createMutation.isPending}
+      confirmLoading={editMutation.isPending}
       okText="Сохранить"
       cancelText="Отмена"
       destroyOnHidden
       afterClose={() => form.resetFields()}
+      footer={(_, { OkBtn, CancelBtn }) => (
+        <Space style={{ width: '100%', justifyContent: 'space-between' }}>
+          <Popconfirm
+            title="Удалить пользователя?"
+            okText="Удалить"
+            cancelText="Отмена"
+            onConfirm={handleDelete}
+          >
+            <Button type="primary">Удалить</Button>
+          </Popconfirm>
+
+          <Space>
+            <OkBtn />
+            <CancelBtn />
+          </Space>
+        </Space>
+      )}
     >
       <UserForm form={form} isEdit={true} initialValues={initialValues} onFinish={() => {}} />
     </Modal>
