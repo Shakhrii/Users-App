59c2ec5a308b095d6186a1c9041dd4882213c4c0
Shakhrii <sh.ramaldanova@gmail.com>
2026-03-25 09:03:44 +0300

    feat: add colors to root styles

--- src/app/styles/GlobalStyle.ts
+++ src/app/styles/GlobalStyle.ts
@@ -1,6 +1,15 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+  :root {
+    --bg-color: #000;
+    --accent-color: #3ca37f;
+    --text-color-light: #c2c2c2;
+    --text-color-dark: #848786;
+    --hover-color: #167556;
+    --disabled-color: #d8f0e8;
+  }
+
   *,
   *::before,
   *::after {
