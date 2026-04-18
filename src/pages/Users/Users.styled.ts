bb04f37c3504f2ebf3b6afa3f3f03610f8a49036
Shakhrii <sh.ramaldanova@gmail.com>
2026-04-19 02:32:59 +0300

    feat: add pagination

--- src/pages/Users/Users.styled.ts
+++ src/pages/Users/Users.styled.ts
@@ -8,6 +8,9 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   color: white;
+  align-items: center;
+  justify-content: center;
+  gap: 30px;
 `;
 
 export const StyledButton = styled(Button)`
--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Spin, Alert, Card } from 'antd';
+import { Avatar, Spin, Alert, Card, Pagination } from 'antd';
 import { LoadingOutlined, UserAddOutlined, EditOutlined } from '@ant-design/icons';
 import { User, useUsersQuery } from '@entities/user';
 import * as S from './Users.styled';
@@ -8,10 +8,15 @@ import { CreateUserModal, EditUserModal } from '@features/user/ui/';
 import Meta from 'antd/es/card/Meta';
 
 const Users = () => {
+  const pageSize = 9;
   const { data, isLoading, isError, error } = useUsersQuery();
   const [isOpenCreateUserForm, setOpenCreateUserForm] = useState(false);
   const [isOpenEditUserForm, setOpenEditUserForm] = useState(false);
   const [selectedUser, setSelectedUser] = useState<User | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const paginatedData = data?.slice((currentPage - 1) * pageSize, currentPage * pageSize);
+  console.log(data?.length);
 
   const openCreateUserForm = () => {
     setOpenCreateUserForm(true);
@@ -44,6 +49,13 @@ const Users = () => {
       <S.StyledButton type="primary" icon={<UserAddOutlined />} onClick={openCreateUserForm}>
         Добавить пользователя
       </S.StyledButton>
+      <Pagination
+        onChange={(page) => setCurrentPage(page)}
+        total={data.length}
+        pageSize={pageSize}
+        defaultCurrent={1}
+        showSizeChanger={false}
+      />
       <CreateUserModal open={isOpenCreateUserForm} onClose={closeCreateUserForm} />
       {selectedUser && (
         <EditUserModal
@@ -53,8 +65,9 @@ const Users = () => {
         />
       )}
       <S.StyledList>
-        {data.map((user) => (
+        {paginatedData?.map((user) => (
           <Card
+            key={user.id}
             style={{ width: 300 }}
             cover={<img draggable={false} alt="user" src={user.avatar} />}
             actions={[<EditOutlined key="edit" onClick={() => userClick(user)} />]}
@@ -67,22 +80,6 @@ const Users = () => {
           </Card>
         ))}
       </S.StyledList>
-      {/* <List
-        dataSource={data}
-        renderItem={(user) => (
-          <List.Item actions={[<EditOutlined key="edit" />, <DeleteOutlined key="delete" />]}>
-            <S.StyledListItem
-              avatar={
-                <S.ClickWrapper onClick={() => userClick(user)}>
-                  <Avatar src={user.avatar} />
-                </S.ClickWrapper>
-              }
-              title={<S.ClickWrapper onClick={() => userClick(user)}>{user.name}</S.ClickWrapper>}
-              description={`Зарегистрирован ${dayjs(user.createdAt).format('DD:MM:YYYY')}`}
-            />
-          </List.Item>
-        )}
-      /> */}
     </S.Container>
   );
 };
